dd07bbf4231824495d9a9831006ce38d08295da9
Pedro Luis Valdivieso <pedro.valdivieso@gmail.com>
2017-05-21 16:26:25 -0400

    Minor interface changes to collection view cell

--- nearby-places/NearbyPlacesListViewCell.swift
+++ nearby-places/NearbyPlacesListViewCell.swift
@@ -13,9 +13,16 @@ class NearbyPlacesListViewCell: UICollectionViewCell {
     @IBOutlet weak var nearbyPlaceImageView: UIImageView!
     @IBOutlet weak var nearbyPlaceLabel: UILabel!
 
-    override func awakeFromNib() {
-        super.awakeFromNib()
-        // Initialization code
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.alpha = 0.5;
+    }
+    
+    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.alpha = 1;
+    }
+    
+    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.alpha = 1;
     }
 
 }
